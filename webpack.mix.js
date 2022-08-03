7bb48f009047f9452efac021ab1bdafc270834b7
africamatji <africamatji@gmail.com>
2022-08-03 16:15:25 +0200

    webpack config

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+mix.js("resources/js/app.js", "public/js")
+    .vue()
+    .postCss("resources/css/app.css", "public/css", [
         //
-    ]);
+    ])
+    .copy('resources/img/', 'public/' );
+
+mix.webpackConfig({
+    output: {
+        chunkFilename: "js/[name].js?id=[chunkhash]",
+    },
+});
